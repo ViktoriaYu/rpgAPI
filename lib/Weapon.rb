c4449b0d8697c375065672fae5b37e51ffa4f404
ViktoriaYu <yudina.viktoria@gmail.com>
2023-04-10 12:33:15 +0300

    Added weapon

---
+++ lib/Weapon.rb
@@ -0,0 +1,11 @@
+class Weapon
+    attr_accessor :name
+    attr_accessor :coefAD
+
+    def initialize(initial_name, initial_coefAD)
+        @name = initial_name
+        @coefAD = initial_coefAD
+    end
+
+end
+
