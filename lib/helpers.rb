b62f9fad191baef7d9c28094317d70b18da4f4bf
nevermind322 <alexfilonov2015@yandex.ru>
2023-04-15 17:55:24 +0300

    add header file

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Game
-  def turn(entity1, entity2)
+  def self.turn(entity1, entity2)
     if entity1.speed > entity2.speed
       print "#{entity1.name} moves... "
       entity1.take_turn
@@ -33,7 +33,7 @@ module Game
     end
   end
 
-  def display_hp2(entity1, entity2)
+  def self.display_hp2(entity1, entity2)
     print format("⌈%19s⌉    ⌈%19s⌉\n", entity1.name, entity2.name)
     print format("⌊mana %14d⌋    ⌊mana %14d⌋\n", entity1.mana, entity2.mana)
     white_hearts1 = (entity1.current_hp / entity1.maximum_hp * 10).round
@@ -42,7 +42,7 @@ module Game
     print '♥' * [white_hearts2, 0].max, '♡' * [10 - white_hearts2, 10].min, format("%5d/%5d\n", entity2.current_hp, entity2.maximum_hp)
   end
 
-  def battle2(entity1, entity2)
+  def self.battle2(entity1, entity2)
     print "starting battle between #{entity1.name} and #{entity2.name}...\n"
     display_hp2(entity1, entity2)
     turn_count = 1
@@ -60,7 +60,7 @@ module Game
 
   end
 
-  def battle
+  def self.battle
     $entities.sort
     $entities.each { |entity| entity.take_turn }
   end
