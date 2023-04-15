c04100dbf3491a2a1b1dfb96dcde5211b3890664
Ni Tail <wrumwrum@list.ru>
2023-04-15 13:26:07 +0300

    Added the skeleton of the multiplayer mode

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -42,23 +42,26 @@ module Game
     print '♥' * [white_hearts2, 0].max, '♡' * [10 - white_hearts2, 10].min, format("%5d/%5d\n", entity2.current_hp, entity2.maximum_hp)
   end
 
-  def battle(entity1, entity2)
+  def battle2(entity1, entity2)
     print "starting battle between #{entity1.name} and #{entity2.name}...\n"
     display_hp2(entity1, entity2)
-    c = 1
+    turn_count = 1
 
-
-    $entities
     while entity1.current_hp.positive? && entity2.current_hp.positive?
-      print "turn #{c}\n"
+      print "turn #{turn_count}\n"
       turn(entity1, entity2)
 
       display_hp2(entity1, entity2)
-      c += 1
+      turn_count += 1
     end
 
     print "🏵 #{entity2.name} won!🏵\n" if entity1.current_hp <= 0
     print "🏵 #{entity1.name} won!🏵\n" if entity2.current_hp <= 0
 
   end
+
+  def battle
+    $entities.sort
+    $entities.each { |entity| entity.take_turn }
   end
+end
