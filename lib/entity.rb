d24a54787304849d606b055e84885a4d3f9d2738
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2023-04-16 13:31:03 +0300

    fix

--- lib/entity.rb
+++ lib/entity.rb
@@ -6,7 +6,6 @@ module Game
   class Entity
     attr_accessor :name, :team, :current_hp, :maximum_hp, :attack_damage, :ability_power, :armor, :magic_resist, :speed, :statuses
 
-
     # @param [String] name
     # @param [Integer] strength
     # @param [Integer] agility
@@ -15,7 +14,6 @@ module Game
       raise ArgumentError, "Strength stats can't be negative" if strength.negative?
       raise ArgumentError, "Agility stats can't be negative" if agility.negative?
       raise ArgumentError, "Intelligence stats can't be negative" if intelligence.negative?
-
 
       @name = name
       @team = team
@@ -32,57 +30,53 @@ module Game
 
     # @param [Float] amount
     # @param [Class] type
-      def take_damage(amount, type)
+    def take_damage(amount, type)
 
       raise ArgumentError, "Can't deal negative damage!" if amount.negative?
 
       case type
       when :physical
-        damage = (if @armor >= 0
-                    amount * 100.0 / (100 + @armor)
-                  else
-                    amount * (2 - 100.0 / (100 - @armor))
-                  end).to_f
+        damage = (
+          if @armor >= 0
+            amount * 100.0 / (100 + @armor)
+          else
+            amount * (2 - 100.0 / (100 - @armor))
+          end).to_f
       when :magic
-        damage = (if @magic_resist >= 0
-                    amount * 100.0 / (100 + @magic_resist)
-                  else
-                    amount * (2 - 100.0 / (100 - @magic_resist))
-                  end).to_f
+        damage = (
+          if @magic_resist >= 0
+            amount * 100.0 / (100 + @magic_resist)
+          else
+            amount * (2 - 100.0 / (100 - @magic_resist))
+          end).to_f
       when :pure
         damage = amount.to_f
       else
         raise ArgumentError, "Unknown damage type"
       end
 
-        @current_hp -= damage
-        print "#{@name} takes #{damage.round(1)} #{type} damage\n"
-      end
+      @current_hp -= damage
+      print "#{@name} takes #{damage.round(1)} #{type} damage\n"
+    end
 
     # @param [Float] amount
     def heal(amount)
 
       raise ArgumentError, "Can't heal negative amount of hp" if amount.negative?
 
-    @current_hp += amount
-    @current_hp = @maximum_hp if @current_hp > @maximum_hp
-    print "#{@name} heals for #{amount} hp\n"
-  end
+      @current_hp += amount
+      @current_hp = @maximum_hp if @current_hp > @maximum_hp
+      print "#{@name} heals for #{amount} hp\n"
+    end
 
-  def take_turn
-    #p @statuses
-    @statuses.select! {|s| s[1] > 0}
-    @statuses.each{|s| s[0].tic(self, s[1]); s[1]-=1}
-  end
+    def add_status(status)
+      @statuses.push([status, status.duration])
+    end
 
-  def add_status(status)
-    @statuses.push([status, status.duration])
+    def take_turn
+      # p @statuses
+      @statuses.select! { |s| s[1] > 0 }
+      @statuses.each { |s| s[0].tic(self, s[1]); s[1] -= 1 }
+    end
   end
-
-  def take_turn
-    #p @statuses
-    @statuses.select! {|s| s[1] > 0}
-    @statuses.each{|s| s[0].tic(self, s[1]); s[1]-=1}
-  end
-end
 end
